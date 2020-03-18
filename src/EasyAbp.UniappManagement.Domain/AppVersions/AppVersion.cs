2770a0f518918ace1ea9d259e574aa5c82a193b1
Super <47396430@qq.com>
2020-03-18 18:39:18 +0800

    Add App and AppVersion aggregate roots.

---
+++ src/EasyAbp.UniappManagement.Domain/AppVersions/AppVersion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using JetBrains.Annotations;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
+
+namespace EasyAbp.UniappManagement.AppVersions
+{
+    public class AppVersion : FullAuditedAggregateRoot<Guid>, IMultiTenant
+    {
+        public virtual Guid? TenantId { get; protected set; }
+        
+        public virtual Guid AppId { get; protected set; }
+        
+        public virtual long BuildNumber { get; protected set; }
+
+        [NotNull]
+        public virtual string Tag { get; protected set; }
+
+        [CanBeNull]
+        public virtual string WgtUrl { get; protected set; }
+        
+        [CanBeNull]
+        public virtual string PkgUrl { get; protected set; }
+        
+        public virtual bool IsLatest { get; protected set; }
+        
+        protected AppVersion() { }
+
+        public AppVersion(
+            Guid id,
+            Guid? tenantId,
+            Guid appId,
+            long buildNumber,
+            [NotNull] string tag,
+            [CanBeNull] string wgtUrl,
+            [CanBeNull] string pkgUrl,
+            bool isLatest) : base(id)
+        {
+            TenantId = tenantId;
+            AppId = appId;
+            BuildNumber = buildNumber;
+            Tag = tag;
+            WgtUrl = wgtUrl;
+            PkgUrl = pkgUrl;
+            IsLatest = isLatest;
+        }
+    }
+}
---
+++ src/EasyAbp.UniappManagement.Domain/Apps/App.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using JetBrains.Annotations;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
+
+namespace EasyAbp.UniappManagement.Apps
+{
+    public class App : FullAuditedAggregateRoot<Guid>, IMultiTenant
+    {
+        public virtual Guid? TenantId { get; protected set; }
+        
+        [NotNull]
+        public virtual string Name { get; protected set; }
+        
+        [CanBeNull]
+        public virtual string Description { get; protected set; }
+        
+        [CanBeNull]
+        public virtual string IconImageUrl { get; protected set; }
+        
+        public virtual bool IsAvailable { get; protected set; }
+        
+        protected App() { }
+
+        public App(
+            Guid id,
+            Guid? tenantId,
+            [NotNull] string name,
+            [CanBeNull] string description,
+            [CanBeNull] string iconImageUrl,
+            bool isAvailable) : base(id)
+        {
+            TenantId = tenantId;
+            Name = name;
+            Description = description;
+            IconImageUrl = iconImageUrl;
+            IsAvailable = isAvailable;
+        }
+    }
+}
